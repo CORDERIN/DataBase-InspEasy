4c2aaa2078e86edc542a0165a91fb782b4b65e39
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-09-14 23:17:30 -0300

    tela de login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <main class="login">
+        <div class="login_container">
+            <h1 class="login_title">Login</h1>
+
+            <form action="" class="login_form">
+
+                <input class="login_input" type="email" placeholder="e-mail">
+                <span class="login_input-border"></span>
+                <input class="login_input" type="password" placeholder="senha">
+                <span class="login_input-border"></span>
+                <button class="login_submit">Login</button>
+                <button class="login_submit">Cadastrar</button>
+                <a class="login_reset" href="">Esqueci a senha</a>
+
+            </form>
+        </div>
+    </main>
+</body>
+</html>
